98dc44cc62341bad622b477c0eab2b568fb4eed2
Max Pumperla <max.pumperla@numberfour.eu>
2015-08-13 14:27:31 +0200

    Setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='elephas',
+      version='0.1',
+      description='Deep learning on Spark with Keras',
+      url='http://github.com/maxpumperla/elephas',
+      author='Max Pumperla',
+      author_email='max.pumperla@googlemail.com',
+      license='MIT',
+      packages=['elephas'],
+      zip_safe=False)
