14832783b67242035287981bb1c2cdd6536fa289
Max Pumperla <max.pumperla@googlemail.com>
2015-09-14 16:01:34 +0200

    Adapter interface for ml

---
+++ elephas/ml/adapter.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+import numpy as np
+
+def to_data_frame(sc, features, labels):
+    return df
+
+def from_data_frame(rdd, categorical=False, nb_classes=None):
+    return features, labels
+
+def df_to_simple_rdd(df):
+    return rdd
+
