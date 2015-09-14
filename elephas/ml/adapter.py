da0ea0fc0cb338bcfcf7934264fff937d28d0fcb
Max Pumperla <max.pumperla@googlemail.com>
2015-09-14 16:54:40 +0200

    Minor typo

--- elephas/ml/adapter.py
+++ elephas/ml/adapter.py
@@ -1,13 +1,24 @@
 from __future__ import absolute_import
 
+from pyspark.sql import SQLContext()
+from pyspark.mllib.regression import LabeledPoint
+
+from ..utils.rdd_utils import from_labeled_point, to_labeled_point, lp_to_simple_rdd
+
 import numpy as np
 
-def to_data_frame(sc, features, labels):
+def to_data_frame(sc, features, labels, categorical=False):
+    lp_rdd = to_labeled_point(sc, features, labels, categorical)
+    sqlContext = SQLContext(sc)
+    df = sqlContext.createDataFrame(lp_rdd)
     return df
 
-def from_data_frame(rdd, categorical=False, nb_classes=None):
-    return features, labels
+def from_data_frame(df, categorical=False, nb_classes=None):
+    lp_rdd = df.rdd.map(lambda row: LabeledPoint(row.label, row.features))
+    result = from_labeled_point(lp_rdd, categorical, nb_classes)
+    return result
 
 def df_to_simple_rdd(df):
+    lp_rdd = df.rdd.map(lambda row: LabeledPoint(row.label, row.features))
+    rdd = lp_to_simple_rdd(lp_rdd)
     return rdd
-
