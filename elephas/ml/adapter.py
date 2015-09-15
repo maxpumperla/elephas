8258a9520c48c7b9ad8772ea84e8d42dd5ea4da4
Max Pumperla <max.pumperla@googlemail.com>
2015-09-15 15:18:05 +0200

    Removed brackets from SQLcontext

--- elephas/ml/adapter.py
+++ elephas/ml/adapter.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-from pyspark.sql import SQLContext()
+from pyspark.sql import SQLContext
 from pyspark.mllib.regression import LabeledPoint
 from ..utils.rdd_utils import from_labeled_point, to_labeled_point, lp_to_simple_rdd
 
