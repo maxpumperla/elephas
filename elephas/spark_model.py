94a65aed5a5e9a09c8979a4e807dbaca6899072e
Max Pumperla <max.pumperla@googlemail.com>
2015-11-11 14:10:55 +0100

    Integration of distributed hogwild

--- elephas/spark_model.py
+++ elephas/spark_model.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 import numpy as np
 import sys
 from itertools import tee
-from keras.models import Sequential, model_from_yaml
+from keras.models import Sequential, model_from_yaml, slice_X
 from pyspark.rdd import RDD
 
 from threading import Thread
@@ -14,19 +14,22 @@ from flask import Flask, request
 import urllib2
 import urlparse
 
-
+from .optimizers import Optimizer
 from .utils.rwlock import RWLock
-from .utils.functional_utils import add_params, subtract_params, get_neutral, divide_by
+from .utils.functional_utils import add_params, subtract_params, get_neutral
 from .utils.rdd_utils import lp_to_simple_rdd
 from .mllib.adapter import to_matrix, from_matrix, to_vector, from_vector
 
 
 class SparkModel(object):
-    def __init__(self, sc, master_network, optimizer, mode='asynchronous', *args, **kwargs):
+    def __init__(self, sc, master_network, optimizer, mode='asynchronous', frequency='epoch', num_workers=4,  *args, **kwargs):
         self.spark_context = sc
         self.master_network = master_network
         self.optimizer = optimizer
         self.mode = mode
+        self.frequency = frequency
+        self.num_workers = num_workers
+
         self.weights = master_network.get_weights()
         self.pickled_weights = None
         self.lock = RWLock()
@@ -82,19 +85,22 @@ class SparkModel(object):
 
         @app.route('/parameters', methods=['GET'])
         def get_parameters():
-            self.lock.acquire_read()
+            if self.mode == 'asynchronous':
+                self.lock.acquire_read()
             self.pickled_weights = pickle.dumps(self.weights, -1)
             pickled_weights = self.pickled_weights
-            self.lock.release()
+            if self.mode == 'asynchronous':
+                self.lock.release()
             return pickled_weights
-
 
         @app.route('/update', methods=['POST'])
         def update_parameters():
             delta = pickle.loads(request.data)
-            self.lock.acquire_write()
+            if self.mode == 'asynchronous':
+                self.lock.acquire_write()
             self.weights = add_params(self.weights,delta)
-            self.lock.release()
+            if self.mode == 'asynchronous':
+                self.lock.release()
             return 'Update done'
 
         print 'Listening to localhost:5000...'
@@ -103,48 +109,36 @@ class SparkModel(object):
     def predict(data):
         return self.master_network.predict(data)
 
-    def train(self, rdd, nb_epoch=10, batch_size=32, verbose=0, validation_split=0.1, num_workers=8):
-        if self.mode == 'synchronous':
-            self.train_synch(rdd, nb_epoch, batch_size, verbose, validation_split, num_workers)
-        elif self.mode == 'asynchronous':
-            self.train_async(rdd, nb_epoch, batch_size, verbose, validation_split, num_workers)
-        elif self.mode == 'dogwild':
-            self.train_async(rdd, nb_epoch, batch_size, verbose, validation_split, num_workers)
+    def train(self, rdd, nb_epoch=10, batch_size=32, verbose=0, validation_split=0.1):
+        if self.mode in ['asynchronous', 'synchronous', 'hogwild']:
+            self._train(rdd, nb_epoch, batch_size, verbose, validation_split)
         else:
-            print 'Choosse from one of the three available modes: asynchronous, synchronous or dogwild'
+            print 'Choose from one of the three available modes: asynchronous, synchronous or hogwild'
 
-    def train_synch(self, rdd, nb_epoch=10, batch_size=32, verbose=0, validation_split=0.1, num_workers=8):
+    def _train(self, rdd, nb_epoch=10, batch_size=32, verbose=0, validation_split=0.1):
+        if self.mode in ['asynchronous', 'hogwild']:
+            self.start_server()
 
-        rdd = rdd.repartition(num_workers)
-
+        rdd = rdd.repartition(self.num_workers)
         yaml = self.master_network.to_yaml()
         parameters = self.spark_context.broadcast(self.master_network.get_weights())
         train_config = self.get_train_config(nb_epoch, batch_size, verbose, validation_split)
 
-        worker = SparkWorker(yaml, parameters, train_config)
-        results = rdd.mapPartitions(worker.train)
-
-        null_element = get_neutral(results.first())
-        new_parameters = divide_by(results.fold(null_element, add_params), num_workers)
+        if self.mode in ['asynchronous', 'hogwild']:
+            worker = AsynchronousSparkWorker(yaml, train_config, self.frequency)
+            results = rdd.mapPartitions(worker.train).collect()
+            new_parameters = self.get_server_weights()
+        elif self.mode == 'synchronous':
+            worker = SparkWorker(yaml, parameters, train_config)
+            results = rdd.mapPartitions(worker.train)
+            null_element = get_neutral(results.first())
+            new_parameters = results.fold(null_element, add_params)
 
         self.master_network.set_weights(new_parameters)
 
-    def train_async(self, rdd, nb_epoch=10, batch_size=32, verbose=0, validation_split=0.1, num_workers=8):
+        if self.mode in ['asynchronous', 'hogwild']:
+            self.stop_server()
 
-        self.start_server()
-
-        rdd = rdd.repartition(num_workers)
-        yaml = self.master_network.to_yaml()
-        parameters = self.spark_context.broadcast(self.master_network.get_weights())
-        train_config = get_train_config(nb_epoch, batch_size, verbose, validation_split)
-
-        worker = AsynchronousSparkWorker(yaml, parameters, train_config)
-        results = rdd.mapPartitions(worker.train).collect()
-
-        new_parameters = self.get_server_weights()
-        self.master_network.set_weights(new_parameters)
-
-        self.stop_server()
 
 class SparkWorker(object):
     def __init__(self, yaml, parameters, train_config):
@@ -160,16 +154,19 @@ class SparkWorker(object):
 
         model = model_from_yaml(self.yaml)
         model.set_weights(self.parameters.value)
-        # Only start training if there's more than one batch of data
+        weights_before_training = model.get_weights()
         if X_train.shape[0] > self.train_config.get('batch_size'):
             model.fit(X_train, y_train, show_accuracy=True, **self.train_config)
-        yield model.get_weights()
+        weights_after_training = model.get_weights()
+        deltas = subtract_params(weights_after_training, weights_before_training)
+        yield deltas
+
 
 class AsynchronousSparkWorker(object):
-    def __init__(self, yaml, parameters, train_config):
+    def __init__(self, yaml, train_config, frequency):
         self.yaml = yaml
-        self.parameters = parameters
         self.train_config = train_config
+        self.frequency = frequency
 
     def train(self, data_iterator):
         feature_iterator, label_iterator = tee(data_iterator,2)
@@ -181,26 +178,47 @@ class AsynchronousSparkWorker(object):
         model.set_weights(initial_weights)
 
         nb_epoch = self.train_config['nb_epoch']
-        self.train_config['nb_epoch'] = 1
-        for epoch in range(nb_epoch):
-            weights_before_training = model.get_weights()
-            model.fit(X_train, y_train, show_accuracy=True, **self.train_config)
-            weights_after_training = model.get_weights()
+        batch_size = self.train_config.get('batch_size')
+        nb_batch = int(np.ceil(size/float(batch_size)))
+        nb_train_sample = len(X_train[0])
+        index_array = np.arange(nb_train_sample)
+        batches = [(i*batch_size, min(size, (i+1)*batch_size)) for i in range(0, nb_batch)]
+        if self.frequency == 'epoch':
+            for epoch in range(nb_epoch):
+                weights_before_training = model.get_weights()
+                if X_train.shape[0] > batch_size:
+                    model.fit(X_train, y_train, show_accuracy=True, **self.train_config)
+                weights_after_training = model.get_weights()
+                deltas = subtract_params(weights_after_training, weights_before_training)
+                self.put_deltas_to_server(deltas)
+        elif self.frequency == 'batch':
+            for epoch in range(nb_epoch):
+                if X_train.shape[0] > batch_size:
+                    for (batch_start, batch_end) in batches:
+                        weights_before_training = model.get_weights()
+                        batch_ids = index_array[batch_start:batch_end]
+                        X = slice_X(X_train, batch_ids)
+                        y = slice_X(y_train, batch_ids)
+                        model.train_on_batch(X,y)
+                        # TODO
+                        # model.fit(X_train, y_train, show_accuracy=True, **self.train_config)
+                        weights_after_training = model.get_weights()
+                        deltas = subtract_params(weights_after_training, weights_before_training)
+                        self.put_deltas_to_server(deltas)
+        else: 
+            print 'Choose frequency to be either batch or epoch'
+        yield []
 
-            deltas = subtract_params(weights_after_training, weights_before_training)
-            self.put_deltas_to_server(deltas)
-        yield weights_after_training
 
 class SparkMLlibModel(SparkModel):
-
-    def train(self, labeled_points, nb_epoch=10, batch_size=32, verbose=0, validation_split=0.1, num_workers=8, categorical=False, nb_classes=None):
+    def train(self, labeled_points, nb_epoch=10, batch_size=32, verbose=0, validation_split=0.1, categorical=False, nb_classes=None):
         rdd = lp_to_simple_rdd(labeled_points, categorical, nb_classes)
-        self.train_rdd(rdd, nb_epoch, batch_size, verbose, validation_split, num_workers)
+        self.train_rdd(rdd, nb_epoch, batch_size, verbose, validation_split)
 
     def predict(mllib_data):
         if isinstance(mllib_data, Matrix):
             return to_matrix(self.master_network.predict(from_matrix(mllib_data)))
         elif isinstance(mllib_data, Vector):
             return to_vector(self.master_network.predict(from_vector(mllib_data)))
-        else: 
+        else:
             print 'Provide either an MLLib matrix or vector'
