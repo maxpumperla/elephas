95e33e64d019247d51a2594f966991ff5a4c0da6
Max Pumperla <max.pumperla@googlemail.com>
2015-11-18 13:51:05 +0100

    prepare for new tag

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='elephas',
-      version='0.1',
+      version='0.2',
       description='Deep learning on Spark with Keras',
       url='http://github.com/maxpumperla/elephas',
       download_url = 'https://github.com/maxpumperla/elephas/tarball/0.1',
