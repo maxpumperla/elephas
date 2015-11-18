29604d6ae65000bef2dbb092b2abdfb87abc0db5
Max Pumperla <max.pumperla@googlemail.com>
2015-11-18 14:48:43 +0100

    include subpackages

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
+from setuptools import find_packages
 
 setup(name='elephas',
-      version='0.2',
+      version='0.2.1',
       description='Deep learning on Spark with Keras',
       url='http://github.com/maxpumperla/elephas',
-      download_url = 'https://github.com/maxpumperla/elephas/tarball/0.1',
+      download_url = 'https://github.com/maxpumperla/elephas/tarball/0.2.1',
       author='Max Pumperla',
       author_email='max.pumperla@googlemail.com',
       install_requires=['keras'],
       license='MIT',
-      packages=['elephas'],
+      packages=find_packages(),
       zip_safe=False)
