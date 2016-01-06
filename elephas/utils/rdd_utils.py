fb21b9e4b65b8821d71682b82641bf7b3dafb702
Max Pumperla <max.pumperla@googlemail.com>
2016-01-06 14:21:52 +0100

    doc strings for rdd utils

--- elephas/utils/rdd_utils.py
+++ elephas/utils/rdd_utils.py
@@ -7,11 +7,19 @@ from ..mllib.adapter import to_vector, from_vector
 
 
 def to_simple_rdd(sc, features, labels):
+    '''
+    Convert numpy arrays of features and labels into
+    an RDD of pairs.
+    '''
     pairs = [(x, y) for x, y in zip(features, labels)]
     return sc.parallelize(pairs)
 
 
 def to_labeled_point(sc, features, labels, categorical=False):
+    '''
+    Convert numpy arrays of features and labels into
+    a LabeledPoint RDD
+    '''
     labeled_points = []
     for x, y in zip(features, labels):
         if categorical:
@@ -23,25 +31,32 @@ def to_labeled_point(sc, features, labels, categorical=False):
 
 
 def from_labeled_point(rdd, categorical=False, nb_classes=None):
+    '''
+    Convert a LabeledPoint RDD back to a pair of numpy arrays
+    '''
     features = np.asarray(rdd.map(lambda lp: from_vector(lp.features)).collect())
     labels = np.asarray(rdd.map(lambda lp: lp.label).collect(), dtype='int32')
     if categorical:
         if not nb_classes:
             nb_classes = np.max(labels)+1
         temp = np.zeros((len(labels), nb_classes))
-        for i in range(len(labels)):
-            temp[i, labels[i]] = 1.
+        for i, label in enumerate(labels):
+            temp[i, label] = 1.
         labels = temp
     return features, labels
 
 
 def encode_label(label, nb_classes):
+    ''' one-hot encoding of a label '''
     encoded = np.zeros(nb_classes)
     encoded[label] = 1.
     return encoded
 
 
 def lp_to_simple_rdd(lp_rdd, categorical=False, nb_classes=None):
+    '''
+    Convert a LabeledPoint RDD into an RDD of feature-label pairs
+    '''
     if categorical:
         if not nb_classes:
             labels = np.asarray(lp_rdd.map(lambda lp: lp.label).collect(), dtype='int32')
