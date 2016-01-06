6b475887409477b075acfb493f16efa056e12454
Max Pumperla <max.pumperla@googlemail.com>
2016-01-06 14:27:13 +0100

    Docstrings for ml adapter

--- elephas/ml/adapter.py
+++ elephas/ml/adapter.py
@@ -4,20 +4,30 @@ from pyspark.sql import SQLContext
 from pyspark.mllib.regression import LabeledPoint
 from ..utils.rdd_utils import from_labeled_point, to_labeled_point, lp_to_simple_rdd
 
-import numpy as np
 
 def to_data_frame(sc, features, labels, categorical=False):
+    '''
+    Convert numpy arrays of features and labels into Spark DataFrame
+    '''
     lp_rdd = to_labeled_point(sc, features, labels, categorical)
-    sqlContext = SQLContext(sc)
-    df = sqlContext.createDataFrame(lp_rdd)
+    sql_context = SQLContext(sc)
+    df = sql_context.createDataFrame(lp_rdd)
     return df
 
+
 def from_data_frame(df, categorical=False, nb_classes=None):
+    '''
+    Convert DataFrame back to pair of numpy arrays
+    '''
     lp_rdd = df.rdd.map(lambda row: LabeledPoint(row.label, row.features))
-    result = from_labeled_point(lp_rdd, categorical, nb_classes)
-    return result
+    features, labels = from_labeled_point(lp_rdd, categorical, nb_classes)
+    return features, labels
+
 
 def df_to_simple_rdd(df, categorical=False, nb_classes=None):
+    '''
+    Convert DataFrame into RDD of pairs
+    '''
     lp_rdd = df.rdd.map(lambda row: LabeledPoint(row.label, row.features))
     rdd = lp_to_simple_rdd(lp_rdd, categorical, nb_classes)
     return rdd
