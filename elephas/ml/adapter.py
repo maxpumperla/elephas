89ee4f6fa1ddf3e64b8574f0d5db29148f3adeec
Max Pumperla <max.pumperla@googlemail.com>
2016-02-23 16:03:48 +0100

    ml adapter with flexible column names;

--- elephas/ml/adapter.py
+++ elephas/ml/adapter.py
@@ -24,10 +24,13 @@ def from_data_frame(df, categorical=False, nb_classes=None):
     return features, labels
 
 
-def df_to_simple_rdd(df, categorical=False, nb_classes=None):
+def df_to_simple_rdd(df, categorical=False, nb_classes=None, featuresCol='features', labelCol='label'):
     '''
     Convert DataFrame into RDD of pairs
     '''
-    lp_rdd = df.rdd.map(lambda row: LabeledPoint(row.label, row.features))
+    sqlContext = df.sql_ctx
+    sqlContext.registerDataFrameAsTable(df, "temp_table")
+    selected_df = sqlContext.sql("SELECT {0} AS features, {1} as label from temp_table".format(featuresCol, labelCol))
+    lp_rdd = selected_df.rdd.map(lambda row: LabeledPoint(row.label, row.features))
     rdd = lp_to_simple_rdd(lp_rdd, categorical, nb_classes)
     return rdd
