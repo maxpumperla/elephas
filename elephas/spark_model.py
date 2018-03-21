44b56f79d832b100ca00ef596d6738fa1e3d2575
ivanmontero <ivanspmontero@gmail.com>
2018-03-21 16:57:33 -0700

    Restored nb_epochs to its original value

--- elephas/spark_model.py
+++ elephas/spark_model.py
@@ -304,6 +304,7 @@ class AsynchronousSparkWorker(object):
                 self.train_config['nb_epochs'] = 1
                 if x_train.shape[0] > batch_size:
                     model.fit(x_train, y_train, **self.train_config)
+                self.train_config['nb_epochs'] = nb_epoch
                 weights_after_training = model.get_weights()
                 deltas = subtract_params(weights_before_training, weights_after_training)
                 put_deltas_to_server(deltas, self.master_url)
