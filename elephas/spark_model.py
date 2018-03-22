389ae7a59fe8610340cbf6f8a42654e88a2f4bed
ivanmontero <ivanspmontero@gmail.com>
2018-03-21 19:48:16 -0700

    Changed 'nb_epochs' to 'nb_epoch'

--- elephas/spark_model.py
+++ elephas/spark_model.py
@@ -301,10 +301,10 @@ class AsynchronousSparkWorker(object):
                 weights_before_training = get_server_weights(self.master_url)
                 model.set_weights(weights_before_training)
                 self.train_config['epochs'] = 1
-                self.train_config['nb_epochs'] = 1
+                self.train_config['nb_epoch'] = 1
                 if x_train.shape[0] > batch_size:
                     model.fit(x_train, y_train, **self.train_config)
-                self.train_config['nb_epochs'] = nb_epoch
+                self.train_config['nb_epoch'] = nb_epoch
                 weights_after_training = model.get_weights()
                 deltas = subtract_params(weights_before_training, weights_after_training)
                 put_deltas_to_server(deltas, self.master_url)
