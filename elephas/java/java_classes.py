b7c7caff69ae21d0243cd977685aa75459e0ab95
Max Pumperla <max.pumperla@googlemail.com>
2018-08-29 17:30:38 +0200

    jumpy, plus cleaning

--- elephas/java/java_classes.py
+++ elephas/java/java_classes.py
@@ -4,11 +4,22 @@ from jnius import autoclass
 File = autoclass('java.io.File')
 ClassLoader = autoclass('java.lang.ClassLoader')
 ArrayList = autoclass('java.util.ArrayList')
+String = autoclass('java.lang.String')
 
 System = autoclass('java.lang.System')
 Integer = autoclass('java.lang.Integer')
 Float = autoclass('java.lang.Float')
 Double = autoclass('java.lang.Double')
+
+# JavaCPP
+DoublePointer = autoclass('org.bytedeco.javacpp.DoublePointer')
+FloatPointer = autoclass('org.bytedeco.javacpp.FloatPointer')
+IntPointer = autoclass('org.bytedeco.javacpp.IntPointer')
+
+# Spark
+SparkContext = autoclass('org.apache.spark.SparkContext')
+JavaSparkContext = autoclass('org.apache.spark.api.java.JavaSparkContext')
+SparkConf = autoclass('org.apache.spark.SparkConf')
 
 # ND4J
 Nd4j = autoclass('org.nd4j.linalg.factory.Nd4j')
@@ -19,7 +30,12 @@ DataBuffer = autoclass('org.nd4j.linalg.api.buffer.DataBuffer')
 Shape = autoclass('org.nd4j.linalg.api.shape.Shape')
 BinarySerde = autoclass('org.nd4j.serde.binary.BinarySerde')
 DataTypeUtil = autoclass('org.nd4j.linalg.api.buffer.util.DataTypeUtil')
+NativeOpsHolder = autoclass('org.nd4j.nativeblas.NativeOpsHolder')
+
+# DataVec
+DataSet = autoclass('org.nd4j.linalg.dataset.DataSet')
 
 
+# Import
 KerasModelImport = autoclass('org.deeplearning4j.nn.modelimport.keras.KerasModelImport')
 ElephasModelImport = autoclass('org.deeplearning4j.spark.parameterserver.modelimport.elephas.ElephasModelImport')
