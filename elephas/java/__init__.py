3244cbc512a6b617600a91ec60b65e3e01d2b1dc
Max Pumperla <max.pumperla@googlemail.com>
2018-09-05 15:44:32 +0200

    try catch

--- elephas/java/__init__.py
+++ elephas/java/__init__.py
@@ -1,11 +1,12 @@
 import jnius_config
 import os
 
-jnius_config.add_options('-Dorg.bytedeco.javacpp.nopointergc=true')
-jnius_class_path = os.environ.get('ELEPHAS_CLASS_PATH')
-if not jnius_class_path:
-    print('WARNING: Environment variable ELEPHAS_CLASS_PATH not set.')
-elif not os.path.exists(jnius_class_path):
-    print('WARNING: File not found : {0}'.format(jnius_class_path))
-else:
-    jnius_config.set_classpath('.', jnius_class_path)
+try:
+    jnius_config.add_options('-Dorg.bytedeco.javacpp.nopointergc=true')
+    jnius_class_path = os.environ.get('ELEPHAS_CLASS_PATH')
+    if not jnius_class_path:
+        print('WARNING: Environment variable ELEPHAS_CLASS_PATH not set.')
+    else:
+        jnius_config.set_classpath('.', jnius_class_path)
+except:
+    print("WARNING: Jnius configuration has already been loaded")
