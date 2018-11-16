d17cdc3dc0fa688399ec4e114336470bf62acd17
Max Pumperla <max.pumperla@googlemail.com>
2018-11-16 17:23:51 +0100

    update requirements

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='elephas',
       download_url='https://github.com/maxpumperla/elephas/tarball/0.3',
       author='Max Pumperla',
       author_email='max.pumperla@googlemail.com',
-      install_requires=['cython', 'keras', 'tensorflow', 'hyperas', 'flask', 'six', 'pyspark', 'pydl4j'],
+      install_requires=['cython', 'keras', 'tensorflow', 'hyperas', 'flask', 'six', 'pyspark', 'pydl4j>=0.1.3'],
       packages=find_packages(),
       license='MIT',
       zip_safe=False)
