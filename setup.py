1e8499e1a5a7c27e169ff33f3cd8521c74cf2830
Max Pumperla <max.pumperla@googlemail.com>
2019-01-03 17:27:07 +0100

    extra requirements

--- setup.py
+++ setup.py
@@ -9,7 +9,21 @@ setup(name='elephas',
       download_url='https://github.com/maxpumperla/elephas/tarball/0.4',
       author='Max Pumperla',
       author_email='max.pumperla@googlemail.com',
-      install_requires=['cython', 'keras', 'tensorflow', 'hyperas', 'flask', 'six', 'pyspark', 'pydl4j>=0.1.3'],
+      install_requires=['cython', 'tensorflow', 'keras', 'hyperas', 'flask', 'six', 'pyspark'],
+      extras_require={
+        'java': ['pydl4j>=0.1.3'],
+        'tests': ['pytest', 'pytest-pep8', 'pytest-cov', 'mock']
+    },
       packages=find_packages(),
       license='MIT',
-      zip_safe=False)
+      zip_safe=False,
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3'
+    ])
