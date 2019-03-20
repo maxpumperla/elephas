e0d41b73f1b3a2b702f936e59518596274e0d55c
Max Pumperla <max.pumperla@googlemail.com>
2019-03-20 15:14:15 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='elephas',
-      version='0.4.1',
+      version='0.4.2',
       description='Deep learning on Spark with Keras',
       url='http://github.com/maxpumperla/elephas',
-      download_url='https://github.com/maxpumperla/elephas/tarball/0.4',
+      download_url='https://github.com/maxpumperla/elephas/tarball/0.4.2',
       author='Max Pumperla',
       author_email='max.pumperla@googlemail.com',
       install_requires=['cython', 'tensorflow', 'keras', 'hyperas', 'flask', 'six', 'pyspark'],
