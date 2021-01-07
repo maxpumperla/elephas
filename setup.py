bff103785a66c080df92a5aeb358db5c7940e977
danielenricocahall <danielenricocahall@gmail.com>
2021-01-06 19:50:57 -0500

    update setup

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='elephas',
-      version='0.4.3',
+      version='0.4.5',
       description='Deep learning on Spark with Keras',
       url='http://github.com/maxpumperla/elephas',
-      download_url='https://github.com/maxpumperla/elephas/tarball/0.4.3',
+      download_url='https://github.com/maxpumperla/elephas/tarball/0.4.5',
       author='Max Pumperla',
       author_email='max.pumperla@googlemail.com',
       install_requires=['cython',
