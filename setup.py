c474bb4f2565185dfd89fd06a8fe467fa9c64b2b
danielenricocahall <danielenricocahall@gmail.com>
2022-03-30 19:22:31 -0400

    update pyspark version

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='elephas',
-      version='3.0.0',
+      version='3.1.0',
       description='Deep learning on Spark with Keras',
       url='http://github.com/maxpumperla/elephas',
-      download_url='https://github.com/maxpumperla/elephas/tarball/3.0.0',
+      download_url='https://github.com/maxpumperla/elephas/tarball/3.1.0',
       author='Daniel Cahall',
       author_email='danielenricocahall@gmail.com',
       install_requires=['cython',
                         'tensorflow>=2,!=2.2.*',
                         'flask',
                         'h5py==3.3.0',
-                        'pyspark<3.2'],
+                        'pyspark==3.2'],
       extras_require={
         'tests': ['pytest', 'pytest-pep8', 'pytest-cov', 'pytest-spark', 'mock']
     },
