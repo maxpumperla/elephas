6ad2bfcb6ca9a226127dbd2961d4c881ce1cb889
Yoseph Zuskin <zuskinyoseph@gmail.com>
2022-09-15 08:45:44 -0400

    update version to reflect minor changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='elephas',
-      version='3.3.0',
+      version='3.4.0',
       description='Deep learning on Spark with Keras',
       url='http://github.com/danielenricocahall/elephas',
       download_url='https://github.com/danielenricocahall/elephas/tarball/3.3.0',
